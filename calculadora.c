50152579173b99ca09fbd064cde841e1ce38654f
Raquel <rvo96c4002w8922@gmail.com>
2026-04-27 17:57:50 -0300

    Refactor calculator program to handle operations

--- calculadora.c
+++ calculadora.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-    
-opcao, numero1, numero2;
+int opcao, numero1, numero2, valor;
+
+void main(void) {
 
 printf("-------------------------------\n");
 printf("CALCULADORA");
@@ -13,6 +13,40 @@ printf("[3] - Multipicacao \n");
 printf("[4] - Divisao \n");
 printf("------------------------------\n");
 
+printf("Escolha a opcao acima: ");
+    scanf("%d", &opcao);
 
-  return 0;
+    switch (opcao)
+        {
+            case 1:
+                 printf("Digite o primeiro valor: ");
+                 scanf("%d", &numero1);
+                 printf("Digite o segundo valor: ");
+                scanf("%d", &numero2);
+                valor = numero1 + numero2;
+            break;
+            case 2:
+                 printf("Digite o primeiro valor: ");
+                 scanf("%d", &numero1);
+                 printf("Digite o segundo valor: ");
+                 scanf("%d", &numero2);
+                 valor = numero1 - numero2;
+            break;
+            case 3:
+                 printf("Digite o primeiro valor: ");
+                 scanf("%d", &numero1);
+                 printf("Digite o segundo valor: ");
+                 scanf("%d", &numero2);
+                 valor = numero1 * numero2;
+            break;
+            case 4:
+                 printf("Digite o primeiro valor: ");
+                 scanf("%d", &numero1);
+                 printf("Digite o segundo valor: ");
+                 scanf("%d", &numero2);
+                 valor = numero1 / numero2;
+            break;
+        }
+    
+    printf("o valor total e de %d", valor);
 }

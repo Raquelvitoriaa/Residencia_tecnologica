9e1e061689dcaa332fff2774c78d6d3ce2a33234
Raquel <rvo96c4002w8922@gmail.com>
2026-04-27 17:06:22 -0300

    Atualizar o calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main() {
+int main(void) {
+    
+opcao, numero1, numero2;
+
+printf("-------------------------------\n");
+printf("CALCULADORA");
+printf("-------------------------------\n");
+printf("[1] - Adicao \n");
+printf("[2] - Subtracao \n");
+printf("[3] - Multipicacao \n");
+printf("[4] - Divisao \n");
+printf("------------------------------\n");
+
 
   return 0;
 }
